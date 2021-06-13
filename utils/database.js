fbc7e3cee8123262bf4e7336770e79bb72795b4d
Tibor Vondrasek <tiboyv@seznam.cz>
2021-06-13 15:54:49 +0200

    add database file

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,16 @@
-const { Pool, Client } = require('pg')
-// pools will use environment variables
-// for connection information
-const pool = new Pool()
-pool.query('SELECT NOW()', (err, res) => {
+const { Client } = require('pg')
+
+const client = new Client({
+	user: 'ejphnxlxyfawqt',
+	host: 'ec2-54-220-170-192.eu-west-1.compute.amazonaws.com',
+	database: 'dfnp8hb21h8gul',
+	password: 'd5d716b4c0dba478dc03caf3ffb92c55520935fcfa5a43e6b348203f30ea602a',
+	port: 5432,
+})
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
 	console.log(err, res)
-	pool.end()
+	client.end()
 })
 
 
